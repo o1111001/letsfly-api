0cd6ccd749f77189db9182d30c660b055a000aef
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-05-07 17:46:07 +0300

    feat: Auth routes

---
+++ routes/auth/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const authorized = require('../../policies/authorized');
+
+const {
+  signUp,
+  login,
+  sendCode,
+} = require('../../controllers');
+
+const {
+  signUp: {
+    validateSignUp,
+  },
+  login: {
+    validateEmail,
+    validateCode,
+  },
+} = require('../../middlewares/auth');
+
+router.post('/signup',
+  validateSignUp,
+  (req, res) => signUp(req, res),
+);
+
+router.post('/login',
+  validateCode,
+  (req, res) => login(req, res),
+);
+
+router.post('/login/code',
+  validateEmail,
+  (req, res) => sendCode(req, res),
+);
+
+router.get('/verify',
+  authorized,
+  (req, res) => res.send('sdfsdfsdf'),
+);
+
+module.exports = router;
