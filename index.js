98d24f157290f1ac387abce8d215f81e83d33217
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-05-07 17:40:57 +0300

    feat: Base file

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const {
+  PORT,
+} = require('./config/env');
+
+require('./config/db');
+
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/auth');
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.disable('x-powered-by');
+
+
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT,
+  () => console.log(`Server listening on ${PORT} port`));
