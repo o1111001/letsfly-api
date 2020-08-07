f04668339cd8bfaa48a8f6180296b3de7dad7d44
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-08-07 19:31:57 +0300

    feat: Auth

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,10 +2,14 @@ const signUp = require('./signup');
 const login = require('./login');
 const sendCode = require('./sendCode');
 const verify = require('./verify');
+const adminVerify = require('./adminVerify');
+const adminLogin = require('./adminLogin');
 
 module.exports = {
   signUp,
   login,
   sendCode,
   verify,
+  adminLogin,
+  adminVerify,
 };
