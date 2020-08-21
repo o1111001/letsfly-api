ebeebaa6a03b03f9c0fa0ee53adf49d0c67c58ba
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-08-21 22:13:33 +0300

    refactor: Restruct auth logic

---
+++ controllers/auth/user/sendCode.js
@@ -0,0 +1,22 @@
+const { sendCode: service } = require('../../../services/auth');
+
+const { sendError } = require('../../../helpers/responses');
+
+const response = email => ({
+  message: `Code sent to ${email}`,
+  data: {
+    email,
+  },
+});
+
+const sendCode = async (req, res) => {
+  try {
+    const { email } = req.body;
+    await service(email);
+    return res.send(response(email));
+  } catch (error) {
+    return sendError(res, error);
+  }
+};
+
+module.exports = sendCode;
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,59 +1,7 @@
-'use strict';
+const admin = require('./admin');
+const user = require('./user');
 
-const express = require('express');
-const router = express.Router();
-
-const authorized = require('../../policies/authorized');
-const isAdmin = require('../../policies/isAdmin');
-
-const {
-  signUp,
-  login,
-  adminLogin,
-  sendCode,
-  verify,
-  adminVerify,
-} = require('../../controllers');
-
-const {
-  signUp: {
-    validateSignUp,
-  },
-  login: {
-    validateEmail,
-    validateCode,
-  },
-} = require('../../middlewares/auth');
-
-router.post('/signup',
-  validateSignUp,
-  (req, res) => signUp(req, res),
-);
-
-router.post('/login',
-  validateCode,
-  (req, res) => login(req, res),
-);
-
-router.post('/login/code',
-  validateEmail,
-  (req, res) => sendCode(req, res),
-);
-
-router.get('/verify',
-  authorized,
-  (req, res) => verify(req, res),
-);
-
-router.post('/admin/login',
-  authorized,
-  (req, res) => adminLogin(req, res),
-);
-
-router.get('/admin/verify',
-  authorized,
-  isAdmin,
-  (req, res) => adminVerify(req, res),
-);
-
-module.exports = router;
+module.exports = {
+  admin,
+  user,
+};
