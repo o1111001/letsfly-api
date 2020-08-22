d3d7f6cf0f43adf4db2f494c23dff9aa9192ef86
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-08-22 22:58:58 +0300

    feat: Move redis vars in env

--- config/env/development.js
+++ config/env/development.js
@@ -9,4 +9,6 @@ module.exports = {
   SALT_ROUNDS: process.env.SALT_ROUNDS && parseInt(process.env.SALT_ROUNDS, 10) || 7,
   EXPIRES: parseInt(process.env.EXPIRES, 10),
   JWT_SECRET: process.env.JWT_SECRET,
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PORT: parseInt(process.env.REDIS_PORT, 10),
 };
