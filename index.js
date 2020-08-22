2012c76a3cc0730455cf24ca74aec5127664d04d
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-08-22 22:58:20 +0300

    feat: Global error handler

--- index.js
+++ index.js
@@ -4,66 +4,39 @@ const express = require('express');
 const app = express();
 
 const server = require('http').Server(app);
+const { PORT } = require('./config/env');
+
 global.basedir = __dirname;
 require('./config/db');
-
-const { PORT } = require('./config/env');
-const { REDIS_HOST, REDIS_PORT } = require('./config/redis');
-
-const authorizedSocket = require('./policies/authorizedSocket');
-const io = require('socket.io')(server);
-const redis = require('socket.io-redis');
-io.adapter(redis({ host: REDIS_HOST, port: REDIS_PORT }));
-io.use(authorizedSocket);
-global.io = io;
+require('./config/socket')(server);
 require('./realtime');
+
+const { join } = require('path');
 
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const {
-  admin: analyticsAdminRoutes,
-} = require('./routes/analytics');
+  analyticsAdminRoutes,
+  authAdminRoutes,
+  authRoutes,
+  contactsRoutes,
+  contactsFindRoutes,
+  chatsRoutes,
+  personalChatsRoutes,
+  messagesRoutes,
+  usersBioRoutes,
+  usersBanRoutes,
+  usersFindRoutes,
+  withdrawsAdminRoutes,
+  withdrawsRoutes,
+  paymentServicesProvider,
+  filesRoutes,
+} = require('./routes/');
 
-const {
-  admin: authAdminRoutes,
-  user: authRoutes,
-} = require('./routes/auth');
+const { globalErrorHandler } = require('./helpers/errors');
 
-const {
-  contacts: contactsRoutes,
-  find: contactsFindRoutes,
-} = require('./routes/contacts');
-
-const {
-  chats: chatsRoutes,
-  personal: personalChatsRoutes,
-} = require('./routes/chats');
-
-const {
-  messages: messagesRoutes,
-} = require('./routes/messages');
-
-const {
-  bio: usersBioRoutes,
-  ban: usersBanRoutes,
-  find: usersFindRoutes,
-} = require('./routes/user');
-
-const {
-  admin: withdrawsAdminRoutes,
-  user: withdrawsRoutes,
-} = require('./routes/withdraws');
-
-// payment providers routes
-const paymentServicesProvider = require('./routes/payment_services_provider');
-
-// static files routes
-const filesRoutes = require('./routes/files');
-const frontendBuild = require('path').join(__dirname, 'build');
-
-app.use((req, res, next) => { console.log(req.method, req.url); return next(); }); //need to implement logger
 app.use(cors());
 app.use(express.json());
 
@@ -73,8 +46,8 @@ app.disable('x-powered-by');
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
-
 app.use(bodyParser.json());
+
 
 // regular routes
 app.get('/api/health', (req, res) => res.send('Success'));
@@ -103,10 +76,14 @@ app.use('/api/v1/payment_services_provider', paymentServicesProvider);
 // static files routes
 app.use('/files', filesRoutes);
 
+const frontendBuild = join(__dirname, 'build');
 app.use(express.static(frontendBuild));
 app.get(/.*/, (req, res) => {
   res.sendFile('index.html', { root: frontendBuild });
 });
+
+// global error handler
+app.use(globalErrorHandler);
 
 server.listen(PORT,
   () => console.log(`Server listening on ${PORT} port`));
