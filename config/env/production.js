91d3d3e57fffc68059258585c1f6a3ebf466b18e
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-09-28 21:18:45 +0300

    feat: DB env

--- config/env/production.js
+++ config/env/production.js
@@ -3,6 +3,8 @@ require('dotenv').config({ path: resolve(__dirname, '../../.env.production') });
 
 module.exports = {
   PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
