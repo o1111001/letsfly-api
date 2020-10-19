d0ad44f91acca37e2a88f1ff8694a863ec992c6e
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-10-19 14:40:56 +0300

    feat: Portgres host

--- config/env/production.js
+++ config/env/production.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 module.exports = {
   production: true,
   PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
