43a70b90953d24dd19afdcf9c9c930377732cedd
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-11-16 21:14:29 +0200

    fix: Delete tokens

--- migrations/20201113135931_refresh-token.js
+++ migrations/20201113135931_refresh-token.js
@@ -1,16 +1,11 @@
-exports.up = knex => {
-  knex('tokens')
-    .del()
-    .then(() => knex.schema
-      .alterTable('tokens', t => {
-        // t.string('token').notNull().alter();
-        t.renameColumn('token', 'access');
-        t.string('refresh').notNull();
-        t.string('fingerprint').notNull();
-        t.dateTime('refreshExpire').notNull();
-      }),
-    );
-};
+exports.up = knex => knex.schema
+  .alterTable('tokens', t => {
+    t.renameColumn('token', 'access');
+    t.string('refresh').notNull();
+    t.string('fingerprint').notNull();
+    t.dateTime('refreshExpire').notNull();
+  });
+
 exports.down = knex => knex.schema.alterTable('tokens', t => {
   t.dropColumn('refresh');
   t.renameColumn('access', 'token');
