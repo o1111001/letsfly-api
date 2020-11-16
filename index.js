d5e6acd46441ae170ed725be2f99ad274e623c0c
Yaroslav Chugai <yaroslavchugai@gmail.com>
2020-11-16 12:45:33 +0200

    fix: Cors

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const {
 
 const { globalErrorHandler } = require('./helpers/errors');
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 // app.use(cors({
 //   origin: 'http://localhost:3000',
